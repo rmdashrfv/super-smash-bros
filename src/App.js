4b39c038c7041352e313eb2e6697fc5aea56ab58
Michael Law <michaellaw@gmx.com>
2022-04-05 13:14:44 -0400

    Created fighter screen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
        <Fighter name={'Roy'} color={'red'} />
        <Fighter name={'Fox'} color={'green'} />
        <Fighter name={'Ganondorf'} color={'purple'} />
+       <Fighter name={'Sheik'} color={'lightskyblue'} />
+     </div>
+     <div className="fighter-screen">
+       <h2>Roy</h2>
+        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+       <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatem pariatur. Quos quasi repudiandae, maxime reiciendis labore cupiditate, eligendi similique ratione, dolor est excepturi recusandae voluptas odio ipsam reprehenderit debitis!
+       </p>
      </div>
     </div>
   );
