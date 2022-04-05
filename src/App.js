65627294db135d1910307470edff59514215d799
Michael Law <michaellaw@gmx.com>
2022-04-05 13:25:09 -0400

    Created fighter screen component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Fighter from './components/Fighter';
+import FighterScreen from './components/FighterScreen';
 
 function App() {
   return (
@@ -11,13 +12,7 @@ function App() {
        <Fighter name={'Ganondorf'} color={'purple'} />
        <Fighter name={'Sheik'} color={'lightskyblue'} />
      </div>
-     <div className="fighter-screen">
-       <h2>Roy</h2>
-        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
-       <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatem pariatur. Quos quasi repudiandae, maxime reiciendis labore cupiditate, eligendi similique ratione, dolor est excepturi recusandae voluptas odio ipsam reprehenderit debitis!
-       </p>
-     </div>
+     <FighterScreen />
     </div>
   );
 }
---
+++ src/components/FighterScreen.js
@@ -0,0 +1,13 @@
+const FighterScreen = () => {
+  return(
+    <div className="fighter-screen">
+      <h2>Roy</h2>
+      <img width="100" height="100" src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatem pariatur. Quos quasi repudiandae, maxime reiciendis labore cupiditate, eligendi similique ratione, dolor est excepturi recusandae voluptas odio ipsam reprehenderit debitis!
+       </p>
+    </div>
+  )
+}
+
+export default FighterScreen;
