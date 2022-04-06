54ab72b705ea25c53ffe80fb97581e7c65083f93
Michael Law <michaellaw@gmx.com>
2022-04-06 11:29:50 -0400

    Designed fighter screen

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -3,10 +3,10 @@ const FighterScreen = ({isVisible, setVisible, selectedCharacter}) => {
     <div onClick={() => { setVisible(false) }} className="fighter-screen" style={{display: isVisible ? "block" : "none"}}>
       <span className="name-bg">{selectedCharacter.name}</span>
       <h2 className="name-fg">{selectedCharacter.name}</h2>
-      <img className="char-portrait" width="100" height="100" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
-      <p className="fighter-desc">
+      <img className="char-portrait" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
+      {/* <p className="fighter-desc">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatem pariatur. Quos quasi repudiandae, maxime reiciendis labore cupiditate, eligendi similique ratione, dolor est excepturi recusandae voluptas odio ipsam reprehenderit debitis!
-       </p>
+       </p> */}
     </div>
   )
 }
